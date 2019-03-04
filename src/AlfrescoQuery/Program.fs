1a001ed0da4ff77286debab6ef1e178bef18a921
wk-j <somnuk.wk@gmail.com>
2019-03-05 01:02:39 +0700

    Test query

--- src/AlfrescoQuery/Program.fs
+++ src/AlfrescoQuery/Program.fs
@@ -1,8 +1,58 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
 
-open System
+type Options = {
+    Path: string
+    Query: string
+    Host: string
+    User: string
+    Password: string
+}
+
+let rec parseArgs args options =
+    match args with
+    | "--host" :: xs ->
+        match xs with
+        | value :: xss ->
+            let newOptions = { options with User = value}
+            parseArgs xss options
+        | _ -> parseArgs xs options
+    | "--user" :: xs ->
+        match xs with
+        | value :: xss ->
+            let newOptions = { options with User = value }
+            parseArgs xss options
+        | _ -> parseArgs xs options
+    | "--password" :: xs ->
+        match xs with
+        | value :: xss ->
+            let newOptions = { options with Password = value }
+            parseArgs xss options
+        | _ -> parseArgs xs options
+    | "--query" :: xs ->
+        match xs with
+        | value :: xss ->
+            let newOptions = { options with Query = value }
+            parseArgs xss newOptions
+        | _ -> parseArgs xs options
+    | "--path" :: xs ->
+        match xs with
+        | value :: xss ->
+            let newOptions = { options with Path = value }
+            parseArgs xss newOptions
+        | _ -> parseArgs xs options
+    | _ -> options
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    let args = List.ofArray argv
+    let init =
+        { Path  = ""
+          Query = ""
+          User  = "admin"
+          Password = "admin"
+          Host = "http://localhost:8082" }
+
+    let options = parseArgs args init
+    printfn "%A" options
+
+    0
